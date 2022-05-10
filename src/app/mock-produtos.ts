45e89c0f4b609494cfbe2283b98817e6be0b15e8
Fábio <fcanedo2001@gmail.com>
2022-05-10 20:57:30 -0300

    Modificando o CSS

---
+++ src/app/mock-produtos.ts
@@ -0,0 +1,14 @@
+import { Produto } from "./produto";
+
+export const PRODUTOS: Produto[] = [
+  { id: 1, name: 'Camisa do São Paulo', preco: 299, qtdEstoque: 45, marca: 'Adidas'},
+  { id: 2, name: 'Camisa do Fluminense', preco: 250, qtdEstoque: 48, marca: 'Adidas'},
+  { id: 3, name: 'Bermuda do Corinthians', preco: 145, qtdEstoque: 32, marca: 'Nike' },
+  { id: 4, name: 'Camisa do Bragantino', preco: 135, qtdEstoque: 81, marca: 'Nike' },
+  { id: 5, name: 'Conjunto do Palmeiras', preco: 350, qtdEstoque: 25, marca: 'Adidas' },
+  { id: 6, name: 'Bola de Futebol', preco: 85, qtdEstoque: 27, marca: 'Penalti' },
+  { id: 7, name: 'Kimono de Jiu Jutsu', preco: 285, qtdEstoque: 42, marca: 'Ryan Gracie' },
+  { id: 8, name: 'Gol de miniatura', preco: 575, qtdEstoque: 142, marca: 'Penalti' },
+  { id: 9, name: 'Rede de volei', preco: 1765, qtdEstoque: 84, marca: 'SR Redes Esportivas' },
+  { id: 10, name: 'Bola de Volei', preco: 85, qtdEstoque: 81, marca: 'Nike' }
+];
